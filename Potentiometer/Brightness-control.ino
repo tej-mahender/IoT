4eb642499de1f4e8bb596392ac66c158b3e1ad03
Tej Mahender <148678239+tej-mahender@users.noreply.github.com>
2024-04-30 11:39:45 +0530

    Create Brightness-control.ino

---
+++ Potentiometer/Brightness-control.ino
@@ -0,0 +1,15 @@
+int led=13;
+void setup(){
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+void loop(){
+ int value=analogRead(A0);
+ int brightness=map(value,0,1023,0,255);
+ digitalWrite(led,brightness);
+ Serial.print("Ananlog Value: ");
+ Serial.println(value);
+ Serial.print("Brightness: ");
+ Serial.println(brightness);
+ delay(1000);
+}
